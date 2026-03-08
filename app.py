7cb11e7c92234f127b8b1cbfb388a3dc5b09d054
Shravana2002 <shravananaik1826@gmail.com>
2026-03-08 20:48:23 +0530

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Week 4 Internship Tasks</title>
+<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<h1>Week 4 Internship Tasks</h1>
+
+<p>This website contains internship task projects.</p>
+
+<button onclick="showMessage()">Click Me</button>
+
+<script src="script.js"></script>
+
+</body>
+</html>
 
